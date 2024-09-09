e8668b9f33fab407a3066b3b744ce2e96c8fc0f8
paulkerla <paul.kerlau@gmail.com>
2024-09-09 13:39:53 +0200

    changement lien accueil
    Remplacer home par espace vide pour entrer dans l'accueil par défaut

--- src/main/java/eni/pizza/french/pizz/AppController.java
+++ src/main/java/eni/pizza/french/pizz/AppController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AppController {
 
-    @GetMapping("Home")
+    @GetMapping("")
     public String homePage() {
         return "home";
     }
