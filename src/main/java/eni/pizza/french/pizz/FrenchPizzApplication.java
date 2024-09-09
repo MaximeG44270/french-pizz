dafe7772579591e22511c2e0b2955f6b5047fe27
Maxime.G <gilbert.maxime01@gmail.com>
2024-09-09 12:12:01 +0200

    commit 1
    création project

---
+++ src/main/java/eni/pizza/french/pizz/FrenchPizzApplication.java
@@ -0,0 +1,13 @@
+package eni.pizza.french.pizz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrenchPizzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrenchPizzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eni/pizza/french/pizz/FrenchPizzApplicationTests.java
@@ -0,0 +1,13 @@
+package eni.pizza.french.pizz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FrenchPizzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
