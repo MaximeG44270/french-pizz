4c7d9eddda8323e69e437e248f0d46f4693b81b8
Maxime.G <gilbert.maxime01@gmail.com>
2024-09-12 13:44:30 +0200

    Commit 22
    ajout page-commande/menu

---
+++ scripts-sql/script-produit.sql
@@ -0,0 +1,25 @@
+-- Insertion de 10 pizzas
+INSERT INTO produit (id_produit, nom, description, prix, image_url, TYPE_PRODUIT_id_type_produit) VALUES
+(1, 'Pizza marseillaise', 'Pizza douce et somptueuse', 15.50, 'https://i.postimg.cc/6Qdr7Vqf/pizza-1.png', 1),
+(2, 'Pizza franklin', 'Pizza pour les champions', 17.00, 'https://i.postimg.cc/3JYx7XQH/pizza-2.png', 1),
+(3, 'Pizza frenchie', 'Pizza signature de la FRENCH PIZZ', 18.75, 'https://i.postimg.cc/qBXpmNhD/pizza-3.png', 1),
+(4, 'Pizza Moorea', 'Pizza corse, mauvais caractère mais délicieuse', 16.25, 'https://i.postimg.cc/63Vx1P34/pizza-4.png', 1),
+(5, 'Pizza gourde', 'Pizza de la saison', 19.00, 'https://i.postimg.cc/QCXZLq94/pizza-5.png', 1),
+(6, 'Pizza paris', 'Pizza gourmande', 14.00, 'https://i.postimg.cc/cLKyNCCS/pizza-6.png', 1),
+(7, 'Pizza hot', 'Pizza chaud chaud !!', 16.50, 'https://i.postimg.cc/tgPKYcFv/pizza-7.png', 1),
+(8, 'Pizza terre', 'Pizza de la terre', 15.00, 'https://i.postimg.cc/tTN8zVSh/pizza-8.png', 1),
+(9, 'Pizza du dev', 'Pizza du dev', 17.50, 'https://i.postimg.cc/HnWRvsRF/pizza-9.png', 1),
+(10, 'Pizza argent', 'Pizza du riche', 35.00, 'https://i.postimg.cc/KcQdkBgb/pizza-10.png', 1);
+
+-- Insertion de 10 boissons
+INSERT INTO produit (id_produit, nom, description, prix, image_url, TYPE_PRODUIT_id_type_produit) VALUES
+(11, 'Coca-Cola', 'Boisson gazeuse classique', 4.50, 'https://i.postimg.cc/Gp2Zj20N/coca.png', 2),
+(12, 'Sprite', 'Boisson gazeuse au citron', 5.00, 'https://i.postimg.cc/HsnXd1gm/sprite.png', 2),
+(13, 'Fanta', 'Boisson gazeuse à l\'orange', 4.75, 'https://i.postimg.cc/R0D2pB40/fanta.png', 2),
+(14, 'Orangina', 'Boisson gazeuse à l\'orange', 4.00, 'https://i.postimg.cc/s2kwZD35/orangina.png', 2),
+(15, 'Oasis', 'Boisson exotique', 6.50, 'https://i.postimg.cc/J0W6S7gR/oasis.png', 2),
+(16, 'Pepsi', 'Boisson gazeuse classique', 4.50, 'https://i.postimg.cc/sg8JtDQb/pepsi.png', 2),
+(17, 'Schweppes', 'Boisson gazeuse au agrum', 5.00, 'https://i.postimg.cc/T2CgzYBQ/schweppes.png', 2),
+(18, 'Red Bull', 'Boisson énergisante', 4.75, 'https://i.postimg.cc/4yJvzYqH/red-bull.png', 2),
+(19, '7up', 'Boisson gazeuse au citron', 4.00, 'https://i.postimg.cc/1XNCjnMC/7up.png', 2),
+(20, 'Capri-Sun', 'Jus de fruits frais', 6.50, 'https://i.postimg.cc/bwThTWZG/caprisun.png', 2);
--- src/main/java/eni/pizza/french/pizz/ihm/AppController.java
+++ src/main/java/eni/pizza/french/pizz/ihm/AppController.java
@@ -1,10 +1,23 @@
 package eni.pizza.french.pizz.ihm;
 
+import eni.pizza.french.pizz.bll.ProduitManager;
+import eni.pizza.french.pizz.bo.Produit;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class AppController {
+
+    @Autowired
+    ProduitManager produitManager;
+
+    public AppController(ProduitManager produitManager) {
+        this.produitManager = produitManager;
+    }
 
     //test
     @GetMapping("home")
@@ -13,7 +26,15 @@ public class AppController {
     }
 
     @GetMapping("menu")
-    public String menuPage() {
+    public String menuPage(Model model) {
+        List<Produit> produits = produitManager.getAllProduits();
+
+        model.addAttribute("produits", produits);
+
+//        if (!produits.isEmpty()) {
+//            Produit lastProduit = produit.get(produit.size() - 1);
+//            model.addAttribute("lastProduit", lastProduit);
+//        }
         return "menu";
     }
 }
