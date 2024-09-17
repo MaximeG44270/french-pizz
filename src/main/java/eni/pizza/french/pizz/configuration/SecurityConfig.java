68c81fa19c8a06811c2625bc3747e03b5579245a
Maxime.G <gilbert.maxime01@gmail.com>
2024-09-17 14:04:46 +0200

    Commit 42
    Ajout commande-detail et nettoyage security

--- src/main/java/eni/pizza/french/pizz/configuration/SecurityConfig.java
+++ src/main/java/eni/pizza/french/pizz/configuration/SecurityConfig.java
@@ -57,43 +57,27 @@ public class SecurityConfig {
                                 .requestMatchers("/link").permitAll()
                                 .requestMatchers("/logout").permitAll()
                                 .requestMatchers("/add-user").permitAll()
-                                //toutes les requêtes commençant par vendor (ex: celles liées aux CSS et JS de uikit) sont autorisées
-                                //ainsi SpringSecurity ne bloque plus le chargement d éléments stockés dans vendor
+                                .requestMatchers("/commande-detail").permitAll()
+                                .requestMatchers("/commande-detail/**").permitAll()
                                 .requestMatchers("/vendor/**").permitAll()
                                 .requestMatchers("/style/**").permitAll()
                                 .requestMatchers("/image/**").permitAll()
-                                //Pour toutes les autres requêtes qui ne sont pas explicitement déclarées ci-dessus,
-                                //  alors on peut soit tout autoriser après authentification via .anyRequest().authenticated()
-                                // ou alors plutôt les rejettées par défaut via:
-                                .anyRequest().denyAll()
-                        //ATTENTION: cette dernière requête anyRequest() doit être la dernière instruction!
 
-                        // remarque : hasRole("ADMIN") est utilisable en lieu et place de hasAuthority("ROLE_ADMIN")
-                        //idem avec hasAnyRole("EMPLOYE", "FORMATEUR", "ADMIN")) à la place de hasAnyAuthority("ROLE_EMPLOYE", "ROLE_FORMATEUR", "ROLE_ADMIN")
+                                .anyRequest().denyAll()
                 );
 
-        //Permet de configurer la page avec l'url "/login" pour qu'elle renvoie sur la page login fournie par défaut par Spring Security
-        //et non notre page "/V2/login-page-v2.html"
-        //http.formLogin(Customizer.withDefaults());
-        //ATTENTION: quand on customize une COnnexion personnalisée, la page de DECOnnexion par défaut devra également etre obligatoirement personalisée
-        //Permet ici de conserver en url "/login" le getMapping personnalisé présent dans DemoController renvoyant vers "/login-page.html"
         http.formLogin(form -> form.loginPage("/login")
                 // permet le retour vers la page url  "" en cas de succès de connexion
                 .defaultSuccessUrl("/user-connected"));
 
         HeaderWriterLogoutHandler clearSiteData = new HeaderWriterLogoutHandler(new ClearSiteDataHeaderWriter(ClearSiteDataHeaderWriter.Directive.ALL));
 
-        //Permet ici d'instaurer que l'appel de l'url "/logout" provoquera une redirection et un nettoyage total de tous les éléments de cache mémoire , cookies etc
-        //déposes sur le client
         http.logout((logout) ->
                 logout
                         .logoutRequestMatcher(new AntPathRequestMatcher("/logout", "GET"))
-                        // permet le retour vers la page url  "/login" en cas de succès de déconnexion
                         .logoutSuccessUrl("/login?logout")
                         .addLogoutHandler(clearSiteData)
         );
-
         return http.build();
     }
-
 }
