635145067206501b8f4118badc45297b9fd5f29c
Maxime.G <gilbert.maxime01@gmail.com>
2024-09-18 15:00:42 +0200

    Commit 51
    Correction dans JS du slideShow (index)

--- src/main/resources/static/JavaScript/nos-pizzas.js
+++ src/main/resources/static/JavaScript/nos-pizzas.js
@@ -1,33 +1,56 @@
-const slides = document.querySelectorAll(".slides img")
+const slides = document.querySelectorAll(".slides img");
 let slideIndex = 0;
 let intervalId = null;
 
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
-function initializeSlider(){
-    if(slides.length > 0){
+function initializeSlider() {
+    if (slides.length > 0) {
         slides[slideIndex].classList.add("displaySlide");
-        setInterval(nextSlide, 50000000);
+        intervalId = setInterval(nextSlide, 5000); // Réduisez l'intervalle pour des tests plus rapides
+        updateButtons();
     }
 }
 
-function showSlide(index){
-    if(index >= slides.length){
+function showSlide(index) {
+    if (index >= slides.length) {
         slideIndex = slides.length - 1;
+    } else if (index < 0) {
+        slideIndex = 0;
+    } else {
+        slideIndex = index;
     }
     slides.forEach(slide => {
         slide.classList.remove("displaySlide");
     });
     slides[slideIndex].classList.add("displaySlide");
+    updateButtons();
 }
 
-function prevSlide(){
+function prevSlide() {
     clearInterval(intervalId);
     slideIndex--;
     showSlide(slideIndex);
 }
 
-function nextSlide(){
+function nextSlide() {
     slideIndex++;
     showSlide(slideIndex);
+}
+
+function updateButtons() {
+    const prevButton = document.getElementById("prevButton");
+    const nextButton = document.getElementById("nextButton");
+
+    if (slideIndex === 0) {
+        prevButton.style.display = "none";
+    } else {
+        prevButton.style.display = "inline-block";
+    }
+
+    if (slideIndex === slides.length - 1) {
+        nextButton.style.display = "none";
+    } else {
+        nextButton.style.display = "inline-block";
+    }
 }
